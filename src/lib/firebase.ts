099b353d506b999b34ae549812a9d349a50f9503
sandesh chhetri <thesandeshchhetri@gmail.com>
2025-09-09 17:37:41 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,12 @@
 // This file is intentionally left blank. 
 // The Firebase configuration will be added here by the Firebase CLI during deployment.
 // It is recommended to not store your configuration in source control.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "noteswift-hs7i8",
+  "appId": "1:40404564759:web:f3f3c39df75dd6c70b1552",
+  "storageBucket": "noteswift-hs7i8.firebasestorage.app",
+  "apiKey": "AIzaSyBTH1I435QmdOpKShTOIT6xPguxL6CI468",
+  "authDomain": "noteswift-hs7i8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "40404564759"
+};
