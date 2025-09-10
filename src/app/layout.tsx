da6fc215b8017c04a84f23e90786ab52a8003af7
sandesh chhetri <thesandeshchhetri@gmail.com>
2025-09-10 17:47:45 +0000

    Internal Server Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
